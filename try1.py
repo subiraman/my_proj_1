bb36f445d150fa71c027558c9274ff8bd2e5a850
subiraman <subiksha1987@gmail.com>
2018-03-18 21:19:45 -0700

    first try

---
+++ try1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import sys
+import os
+import subprocess
+import numpy
+import scipy
+import panda as pd
+
+
+##Main##
+########
+
+print "hello world"
+###print sys.path
+#print sys.path[0]
